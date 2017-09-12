6d584de3c5927c97ed0e64854368c4cb945f56f5
Pär Tjärnberg <par.tjarnberg@cygni.se>
2017-09-12 16:49:21 +0200

    Use bash only for calculating age

--- birthdaycalc.py
+++
@@ -1,4 +0,0 @@
-from datetime import date; from dateutil.relativedelta import *
-from dateutil.parser import parse; import sys
-
-print relativedelta(date.today(), parse(sys.argv[2])).years
--- birthdayreminder.sh
+++ birthdayreminder.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-while getopts ":s:b:n:e:" opt; do
+while getopts ":s:b:n:e:u:x:" opt; do
   case $opt in
     s) subject_prefix="$OPTARG"
     ;;
@@ -10,9 +10,24 @@ while getopts ":s:b:n:e:" opt; do
     ;;
     e) email="$OPTARG"
     ;;
+    u) user="$OPTARG"
+    ;;
+    x) slack_hook_url="$OPTARG"
+    ;;
   esac
 done
 
-age="$(python birthdaycalc.py "$name" $birthday)"
+age=$(($(date -jf %s $((`date +%s` - `date -jf %Y-%m-%d "$birthday" +%s`)) +%Y)-1970))
 
 echo "Congratulate $name who turns $age today" | mail -s "$subject_prefix: $name" $email
+
+birthday_icons=(":birthday:" ":sparkles:" ":tada:" ":champagne:" ":cake:" \
+":star2:" ":star:" ":cocktail:" ":tropical_drink:" ":dizzy:")
+size=${#birthday_icons[@]}
+
+payload="{\"username\": \"Polluxian\", \"text\": \"Pssst! Idag är det en speciell dag. Det är $user 's födelsedag! Hurra! ${birthday_icons[$(($RANDOM % $size))]} \
+${birthday_icons[$(($RANDOM % $size))]} ${birthday_icons[$(($RANDOM % $size))]}\", \"icon_emoji\": \":alien:\"}"
+
+echo $payload
+
+curl -X POST --data-urlencode "payload=$payload" $slack_hook_url
