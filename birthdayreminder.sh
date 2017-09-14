6493ca8677cfad3a3029692b38b26b4f960b8e8c
Pär Tjärnberg <par.tjarnberg@cygni.se>
2017-09-14 19:41:14 +0200

    Declare constants in config file

--- birthdayreminder.sh
+++ birthdayreminder.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+source config.sh
 usage () { echo 'HOW TO USE:
 $ birthdayreminder.sh -n <who to celebrate> -b <birthday> -e <email to notify about age> -s <email subject prefix> -u <slack username> -x <slack hook url>
 
@@ -24,25 +24,22 @@ done
 
 # Create and send email
 if date -v 1d > /dev/null 2>&1; then
-  # BSD
+  # handle date as on BSD environment
   age=$(($(date -jf %s $(($(date +%s)-$(date -jf %Y-%m-%d "$birthday" +%s))) +%Y)-1970))
 else
-  # GNU
+  # fallback handle date as on GNU environment
   age=$(($(date --date=@$(($(date +%s)-$(date --date="$birthday" +%s))) +%Y)-1970))
 fi
-eval $age
-email_body=$(<email-message-template)
-email_body=${email_body/NAME/$name}
-email_body=${email_body/AGE/$age}
-echo $email_body | mail -s "$subject_prefix: $name" $email
+mail_payload=$(<email-message-template)
+mail_payload=${mail_payload/$MAIL_NAME_CONTAINER/$name}
+mail_payload=${mail_payload/$MAIL_AGE_CONTAINER/$age}
+echo $mail_payload | mail -s "$subject_prefix: $name" $email
 
 # Create and send slack message
-birthday_icons=(":birthday:" ":sparkles:" ":tada:" ":champagne:" ":cake:" \
-":star2:" ":star:" ":cocktail:" ":tropical_drink:" ":dizzy:")
-size=${#birthday_icons[@]}
-payload=$(<slack-message-template.json)
-payload=${payload/SLACK_USER/$slack_user}
-while [[ $payload == *"RANDOM_SLACK_ICON"* ]]; do
-    payload=${payload/RANDOM_SLACK_ICON/${birthday_icons[$(($RANDOM % $size))]}}
+size=${#SLACK_BIRTHDAY_ICONS[@]}
+slack_payload=$(<slack-message-template.json)
+slack_payload=${slack_payload/$SLACK_USER_CONTAINER/$slack_user}
+while [[ $slack_payload == *"$SLACK_ICON_CONTAINER"* ]]; do
+    slack_payload=${slack_payload/$SLACK_ICON_CONTAINER/${SLACK_BIRTHDAY_ICONS[$(($RANDOM % $size))]}}
 done
-curl -X POST --data-urlencode "payload=$payload" $slack_hook_url
+curl -X POST --data-urlencode "payload=$slack_payload" $slack_hook_url
---
+++ config.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# E-mail config
+readonly MAIL_NAME_CONTAINER="NAME"
+readonly MAIL_AGE_CONTAINER="AGE"
+# Slack config
+readonly SLACK_ICON_CONTAINER="RANDOM_SLACK_ICON"
+readonly SLACK_USER_CONTAINER="SLACK_USER"
+readonly SLACK_BIRTHDAY_ICONS=(":birthday:" ":sparkles:" ":tada:" ":champagne:" ":cake:" \
+":star2:" ":star:" ":cocktail:" ":tropical_drink:" ":dizzy:")
